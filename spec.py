f2eabaa45340e35476f82e693df98ef485b3b41f
hedwig3798 <joey3798@naver.com>
2022-12-11 22:15:59 +0900

    equip.py 수정
    증가항목을 리스트화 하여 보기 편하게 만듬

--- spec.py
+++ spec.py
@@ -1,46 +1,94 @@
-class spec:
-    def __init__(self):
-        self.data = {
-            "MAIN_STAT": 0,
-            "SUB_STAT_1": 0,
-            "SUB_STAT_2": 0,
-            "HP": 0,
-            "MP": 0,
-            "FINAL_DMG": 100,
-            "DMG": 0,
-            "IGR": 0,
-            "CRI_CMG": 0,
-            "CRI_RATE": 0,
-            "MAIN_STAT_P": 0,
-            "ALL_STAT_P": 0,
-        }
+from math import trunc, ceil
 
-    def __add__(self, other):
-        result = spec()
-        for i in result.data.keys():
-            if i == "IGR":
-                result.data[i] = self.data[i] + ((100 - self.data[i]) * (other.data[i] * 0.01))
-            elif i == "FINAL_DMG":
-                result.data[i] = ((self.data[i] * 0.01) * ((100 + other.data[i]) * 0.01)) * 100
-            else:
-                result.data[i] = self.data[i] + other.data[i]
+
+class spec:
+
+    # 캐릭터의 스펙을 저장하는 곳 ( 스텟 창 기중 )
+    def __init__(self):
+        self.level = 0  # 정수
+        self.pure_stat = 0  # 정수
+        self.hp = 0  # 정수
+        self.mp = 0  # 정수
+
+        self.final_dmg = 0  # 퍼센트
+        self.dmg = 0  # 퍼센트
+        self.boss_dmg = 0  # 퍼센트
+        self.igr = 0  # 퍼센트
+        self.cri_dmg = 0  # 퍼센트
+        self.cri_rate = 0  # 퍼센트
+        self.main_stat_p = 0  # 퍼센트
+        self.all_stat_p = 0  # 퍼센트
+        self.mystery = 0
+        self.atk = 0
+        self.atk_p = 0
+        self.atk_sub = 0
+
+        self.display_stat_atk = 0
+        self.boss_stat_atk = 0
+        self.main_stat = 0  # 정수
+        self.sub_stat_1 = 4  # 정수
+        self.sub_stat_2 = 4  # 정수
+
+        self.add_main_stat = 0
+        self.add_sub_stat_1 = 0
+        self.add_sub_stat_2 = 0
+
+        self.weapon_ab = 0
+        self.job_ab = 0
+
+    def set_level(self, num):
+        self.level = num
+        self.pure_stat = 14 + (num * 5)
+
+    def add_final_dmg(self, num):
+        temp = self.final_dmg + 1
+        temp *= 1 + num
+        self.final_dmg = temp - 1
+        # self.final_dmg = round(self.final_dmg, 4)
+
+    def add_igr(self, num):
+        self.igr += (1 - self.igr) * num
+
+    def sub_final_dmg(self, num):
+        temp = self.final_dmg + 1
+        temp /= 1 + num
+        self.final_dmg = temp - 1
+        # self.final_dmg = round(self.final_dmg, 4)
+
+    def sub_igr(self, num):
+        self.igr = (1 - self.igr) / num
+
+    def change_stat_atk(self):
+        cal_stat = ((self.pure_stat + self.main_stat) * (1 + self.main_stat_p + self.all_stat_p)) + self.add_main_stat
+        cal_stat *= 0.01
+        cal_stat = trunc(cal_stat)
+
+        self.display_stat_atk = cal_stat * self.atk * (1 + self.atk_p) * self.weapon_ab * self.job_ab * (1 + self.dmg) * (1 + self.final_dmg)
+
+    def __repr__(self):
+        result = ""
+        result += f"레벨: {self.level} \n"
+        result += f"순스텟: {self.pure_stat} \n"
+        result += f"주스텟: {self.main_stat}\n"
+        result += f"주스텟( %미적용 ): {self.add_main_stat}\n"
+        result += f"부스텟 1: {self.sub_stat_1}\n"
+        result += f"부스텟 2: {self.sub_stat_2}\n"
+        result += f"HP: {self.hp}\n"
+        result += f"공: {self.atk}\n"
+        result += f"공퍼: {self.atk_p} \n"
+        result += f"서브공: {self.atk_sub}\n"
+        result += f"주스펫 퍼: {self.main_stat_p} \n"
+        result += f"올스텟 퍼: {self.all_stat_p}\n"
+        result += f"방무: {self.igr} \n"
+        result += f"보공: {self.boss_dmg} \n"
+        result += f"데미지: {self.dmg} \n"
+        result += f"최종뎀: {self.final_dmg} \n"
+        result += f"뒷스공: {self.display_stat_atk} \n"
         return result
 
 
 if __name__ == "__main__":
-    # spec a + b
-
     a = spec()
-    a.data["ATK"] = 10
-    a.data["IGR"] = 50
-    a.data["FINAL_DMG"] = 50
+    a.set_level(270)
+    print(a)
 
-    b = spec()
-    b.data["ATK"] = 10
-    b.data["FINAL_DMG"] = 50
-    b.data["IGR"] = 50
-
-    c = a + b
-
-    for i in c.data.keys():
-        print(i + ": " + str(c.data[i]))
